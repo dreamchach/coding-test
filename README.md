9f1c0354c0b1002e4167b98ed1e76c74f52d9903
김지영 <kimjiyeong@gimjiyeong-ui-Macmini.local>
2023-06-07 16:03:39 +0900

    1-2. a + b

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 ### 1-1. Hello World
 https://www.acmicpc.net/problem/2557
 
-### 1-2.
+### 1-2. a + b
+https://www.acmicpc.net/problem/1000
+
+### 1-3. 
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+/*
+// 1-1
 console.log('Hello World!')
+*/
+
+// 1-2
+const fs = require('fs')
+const input = fs.readFileSync('./input/ch1/1-2.txt').toString().split('\n')
+// console.log(input)
+// ['1 2']
+const line = input[0].split(' ')
+const a = parseInt(line[0])
+const b = parseInt(line[1])
+console.log(a+b)
